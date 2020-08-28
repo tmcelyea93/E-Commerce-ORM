7d230e82fa4b7d3077700a82e0c2af2cace092fb
tmcelyea93 <tarahmcelyea@gmail.com>
2020-08-27 20:43:48 -0500

    minor fixes and added assocations

--- models/index.js
+++ models/index.js
@@ -6,14 +6,27 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-  foreignKey: 'user_id',
+  foreignKey: 'category_id',
 });
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  as: 'tag_name',
+  foreignKey: 'tag_id'
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  as: 'tag_name',
+  foreignKey: 'tag_id'
+});
 
 module.exports = {
   Product,
